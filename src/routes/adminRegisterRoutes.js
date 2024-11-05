7f85e11c43af5e94d92e517e89223fa884f9942a
Lara Braz <141438738+laranbraz@users.noreply.github.com>
2024-11-05 16:07:36 -0300

    routes admin and uploaded files

--- src/routes/adminRegisterRoutes.js
+++ src/routes/adminRegisterRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const { adminRegister } = require('../controllers/adminRegister/adminRegisterController');
+const checkPermission = require('../middleware/checkPermissionMiddleware');
+
+
 const router = express.Router();
-router.post('/registro', adminRegister);
+
+router.post('/registro', checkPermission('administrador'), adminRegister);
+
 module.exports = router;
--- src/routes/uploadedFilesRoutes.js
+++ src/routes/uploadedFilesRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const {recuperarArquivo} = require('../controllers/uploadedFiles/uploadedFileController');
+const checkPermission = require('../middleware/checkPermissionMiddleware');
 
-
-router.get('/:id', recuperarArquivo);
+router.get('/:id', checkPermission('moderador'), recuperarArquivo);
 
 
 module.exports = router;
